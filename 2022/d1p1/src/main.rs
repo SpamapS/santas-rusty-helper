5a15acb37d9a073e8aac680077a9e768fb191ebb
Clint Byrum <clint@fewbar.com>
2022-12-03 10:57:48 -0800

    Working d1p1 2022

---
+++ 2022/d1p1/src/main.rs
@@ -0,0 +1,130 @@
+/*
+--- Day 1: Calorie Counting ---
+
+Santa's reindeer typically eat regular reindeer food, but they need a lot of magical energy to
+deliver presents on Christmas. For that, their favorite snack is a special type of star fruit that
+only grows deep in the jungle. The Elves have brought you on their annual expedition to the grove
+where the fruit grows.
+
+To supply enough magical energy, the expedition needs to retrieve a minimum of fifty stars by
+December 25th. Although the Elves assure you that the grove has plenty of fruit, you decide to grab
+any fruit you see along the way, just in case.
+
+Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent
+calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star.
+Good luck!
+
+The jungle must be too overgrown and difficult to navigate in vehicles or access from the air; the
+Elves' expedition traditionally goes on foot. As your boats approach land, the Elves begin taking
+inventory of their supplies. One important consideration is food - in particular, the number of
+Calories each Elf is carrying (your puzzle input).
+
+The Elves take turns writing down the number of Calories contained by the various meals, snacks,
+rations, etc. that they've brought with them, one item per line. Each Elf separates their own
+inventory from the previous Elf's inventory (if any) by a blank line.
+
+For example, suppose the Elves finish writing their items' Calories and end up with the following
+list:
+
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+
+This list represents the Calories of the food carried by five Elves:
+
+    The first Elf is carrying food with 1000, 2000, and 3000 Calories, a total of 6000 Calories.
+    The second Elf is carrying one food item with 4000 Calories. The third Elf is carrying food
+    with 5000 and 6000 Calories, a total of 11000 Calories. The fourth Elf is carrying food with
+    7000, 8000, and 9000 Calories, a total of 24000 Calories. The fifth Elf is carrying one food
+    item with 10000 Calories.
+
+In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like
+to know how many Calories are being carried by the Elf carrying the most Calories. In the example
+above, this is 24000 (carried by the fourth Elf).
+
+Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+
+*/
+
+use std::fs;
+
+const TEST_INPUT: &str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+
+fn parse_calories(input: &str) -> Vec<Vec<u32>> {
+    let mut parsed = Vec::new();
+    let mut lines = input.lines();
+    let mut elf_inventory = Vec::new();
+    loop {
+        match lines.next() {
+            None => {
+                parsed.push(elf_inventory);
+                break;
+            },
+            Some(line) => {
+                if line.len() == 0 {
+                    parsed.push(elf_inventory.clone());
+                    elf_inventory.clear();
+                } else {
+                    let calories: u32 = line.parse().unwrap();
+                    elf_inventory.push(calories);
+                }
+            }
+        }
+    }
+    return parsed;
+}
+
+#[test]
+fn test_parse_calories() {
+    let parsed_test: Vec<Vec<u32>> = vec!(
+    
+        vec!(1000,2000,3000),
+        vec!(4000),
+        vec!(5000,6000),
+        vec!(7000,8000,9000),
+        vec!(10000)
+    );
+    assert!(parse_calories(TEST_INPUT) == parsed_test);
+}
+
+fn hungriest_elf(elf_inventory: Vec<Vec<u32>>) -> u32 {
+    return elf_inventory.iter()
+        .map(|this_elf| this_elf.iter().sum())
+        .max().unwrap();
+}
+
+#[test]
+fn test_hungriest_elf() {
+    assert!(hungriest_elf(parse_calories(TEST_INPUT)) == 24000)
+}
+
+fn main() {
+    let buf = fs::read_to_string("2022d1p1.txt").unwrap();
+    let hungriest = hungriest_elf(parse_calories(&buf));
+    println!("The hungriest is {}", hungriest);
+}
