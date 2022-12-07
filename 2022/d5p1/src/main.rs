e043bc5e56a6071c447ab9cd875fbb60fddd672f
Clint Byrum <clint@fewbar.com>
2022-12-07 15:11:16 -0800

    finished d5p2

--- 2022/d5p1/src/main.rs
+++ 2022/d5p1/src/main.rs
@@ -9,10 +9,10 @@ The Elves don't want to interrupt the crane operator during this delicate proced
 
 They do, however, have a drawing of the starting stacks of crates and the rearrangement procedure (your puzzle input). For example:
 
-    [D]    
-[N] [C]    
+    [D]
+[N] [C]
 [Z] [M] [P]
- 1   2   3 
+ 1   2   3
 
 move 1 from 2 to 1
 move 3 from 1 to 3
@@ -23,10 +23,10 @@ In this example, there are three stacks of crates. Stack 1 contains two crates: 
 
 Then, the rearrangement procedure is given. In each step of the procedure, a quantity of crates is moved from one stack to a different stack. In the first step of the above rearrangement procedure, one crate is moved from stack 2 to stack 1, resulting in this configuration:
 
-[D]        
-[N] [C]    
+[D]
+[N] [C]
 [Z] [M] [P]
- 1   2   3 
+ 1   2   3
 
 In the second step, three crates are moved from stack 1 to stack 3. Crates are moved one at a time, so the first crate to be moved (D) ends up below the second and third crates:
 
@@ -56,14 +56,64 @@ The Elves just need to know which crate will end up on top of each stack; in thi
 
 After the rearrangement procedure completes, what crate ends up on top of each stack?
 
+--- Part Two ---
+
+As you watch the crane operator expertly rearrange the crates, you notice the process isn't following your prediction.
+
+Some mud was covering the writing on the side of the crane, and you quickly wipe it away. The crane isn't a CrateMover 9000 - it's a CrateMover 9001.
+
+The CrateMover 9001 is notable for many new and exciting features: air conditioning, leather seats, an extra cup holder, and the ability to pick up and move multiple crates at once.
+
+Again considering the example above, the crates begin in the same configuration:
+
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+Moving a single crate from stack 2 to stack 1 behaves the same as before:
+
+[D]        
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+However, the action of moving three crates from stack 1 to stack 3 means that those three moved crates stay in the same order, resulting in this new configuration:
+
+        [D]
+        [N]
+    [C] [Z]
+    [M] [P]
+ 1   2   3
+
+Next, as both crates are moved from stack 2 to stack 1, they retain their order as well:
+
+        [D]
+        [N]
+[C]     [Z]
+[M]     [P]
+ 1   2   3
+
+Finally, a single crate is still moved from stack 1 to stack 2, but now it's crate C that gets moved:
+
+        [D]
+        [N]
+        [Z]
+[M] [C] [P]
+ 1   2   3
+
+In this example, the CrateMover 9001 has put the crates in a totally different order: MCD.
+
+Before the rearrangement process finishes, update your simulation so that the Elves know where they should stand to be ready to unload the final supplies. After the rearrangement procedure completes, what crate ends up on top of each stack?
+
+
 */
 
 use core::fmt;
-use std::error::Error;
-use std::fmt::{Debug,Formatter};
+use regex::Regex;
+use std::fmt::{Debug, Formatter};
 use std::fs;
 use std::num::ParseIntError;
-use regex::Regex;
 
 const TEST_INPUT: &str = "    [D]    
 [N] [C]    
@@ -81,9 +131,9 @@ type Stack = Vec<Crate>;
 
 #[derive(PartialEq)]
 struct Instruction {
-    n: i32,
-    from: i32,
-    to: i32,
+    n: usize,
+    from: usize,
+    to: usize,
 }
 
 impl Debug for Instruction {
@@ -91,7 +141,6 @@ impl Debug for Instruction {
         write!(f, "move {} from {} to {}", self.n, self.from, self.to)
     }
 }
-
 
 fn parse_drawing(input: &str) -> Result<(Vec<Stack>, Vec<Instruction>), ParseIntError> {
     let mut stacks: Vec<Stack> = Vec::new();
@@ -120,15 +169,13 @@ fn parse_drawing(input: &str) -> Result<(Vec<Stack>, Vec<Instruction>), ParseInt
     let mut instructions = Vec::new();
     let instr_re = Regex::new(r"^move (\d+) from (\d+) to (\d+)$").unwrap();
     for line in instr_string.lines() {
-        let m = instr_re.captures(line).expect("Instruction line won't parse");
-        let n: i32 = m.get(1).unwrap().as_str().parse().unwrap();
-        let from: i32 = m.get(2).unwrap().as_str().parse().unwrap();
-        let to: i32 = m.get(3).unwrap().as_str().parse().unwrap();
-        instructions.push(Instruction {
-            n,
-            from,
-            to,
-        });
+        let m = instr_re
+            .captures(line)
+            .expect("Instruction line won't parse");
+        let n: usize = m.get(1).unwrap().as_str().parse().unwrap();
+        let from: usize = m.get(2).unwrap().as_str().parse().unwrap();
+        let to: usize = m.get(3).unwrap().as_str().parse().unwrap();
+        instructions.push(Instruction { n, from, to });
     }
     Ok((stacks, instructions))
 }
@@ -136,9 +183,21 @@ fn parse_drawing(input: &str) -> Result<(Vec<Stack>, Vec<Instruction>), ParseInt
 fn apply_instructions(stacks: &mut Vec<Stack>, instructions: &Vec<Instruction>) {
     for instruction in instructions.iter() {
         for _i in 0..instruction.n {
-            let this_crate: Crate = stacks[instruction.from as usize - 1].pop().expect("Not enough items to move!");
+            let this_crate: Crate = stacks[instruction.from as usize - 1]
+                .pop()
+                .expect("Not enough items to move!");
             stacks[instruction.to as usize - 1].push(this_crate);
         }
+    }
+}
+
+fn apply_instructions_9001(stacks: &mut Vec<Stack>, instructions: &Vec<Instruction>) {
+    for instruction in instructions.iter() {
+        let from_stack = &mut stacks[instruction.from - 1];
+        let to_move = from_stack.len() - instruction.n;
+        let mut moved_crates: Vec<Crate> = from_stack.split_off(to_move);
+        let to_stack = &mut stacks[instruction.to - 1];
+        to_stack.append(&mut moved_crates);
     }
 }
 
@@ -149,10 +208,26 @@ fn test_instructions() {
     test_stacks.push(vec!['M', 'C', 'D']);
     test_stacks.push(vec!['P']);
     let test_instructions: Vec<Instruction> = vec![
-        Instruction { n: 1, from: 2, to: 1},
-        Instruction { n: 3, from: 1, to: 3},
-        Instruction { n: 2, from: 2, to: 1},
-        Instruction { n: 1, from: 1, to: 2},
+        Instruction {
+            n: 1,
+            from: 2,
+            to: 1,
+        },
+        Instruction {
+            n: 3,
+            from: 1,
+            to: 3,
+        },
+        Instruction {
+            n: 2,
+            from: 2,
+            to: 1,
+        },
+        Instruction {
+            n: 1,
+            from: 1,
+            to: 2,
+        },
     ];
     let test_stacks = test_stacks;
     let (mut parsed_stacks, instructions) = parse_drawing(TEST_INPUT).unwrap();
@@ -162,13 +237,30 @@ fn test_instructions() {
     moved_test_stacks.push(vec!['C']);
     moved_test_stacks.push(vec!['M']);
     moved_test_stacks.push(vec!['P', 'D', 'N', 'Z']);
+    let mut save_parsed_stacks = parsed_stacks.clone();
     apply_instructions(&mut parsed_stacks, &instructions);
     assert_eq!(moved_test_stacks, parsed_stacks);
+    let mut moved_9001_stacks: Vec<Stack> = Vec::new();
+    moved_9001_stacks.push(vec!['M']);
+    moved_9001_stacks.push(vec!['C']);
+    moved_9001_stacks.push(vec!['P', 'Z', 'N', 'D']);
+    apply_instructions_9001(&mut save_parsed_stacks, &instructions);
+    assert_eq!(moved_9001_stacks, save_parsed_stacks);
+}
+
+fn top_stacks(stacks: &mut Vec<Stack>) -> String {
+    stacks
+        .iter_mut()
+        .map(|stack| stack.pop().unwrap())
+        .collect::<String>()
 }
 
 fn main() {
     let buf = fs::read_to_string("2022d5p1.txt").unwrap();
     let (mut parsed_stacks, instructions) = parse_drawing(&buf).unwrap();
+    let mut saved_parsed_stacks = parsed_stacks.clone();
     apply_instructions(&mut parsed_stacks, &instructions);
-    println!("The answer is: {}", parsed_stacks.iter_mut().map(|stack| stack.pop().unwrap()).collect::<String>());
+    println!("The answer is: {}", top_stacks(&mut parsed_stacks));
+    apply_instructions_9001(&mut saved_parsed_stacks, &instructions);
+    println!("9001 Answer is: {}", top_stacks(&mut saved_parsed_stacks));
 }
