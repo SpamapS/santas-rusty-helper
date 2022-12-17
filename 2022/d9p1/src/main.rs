599e9282f1bff6159fc4b67c1ef8132bab55ceea
Clint Byrum <clint@fewbar.com>
2022-12-17 15:03:53 -0800

    reworking

--- 2022/d9p1/src/main.rs
+++ 2022/d9p1/src/main.rs
@@ -249,30 +249,148 @@ Simulate your complete hypothetical series of motions. How many positions does t
 
 */
 
-use std::fs;
+use std::{fs, collections::HashSet};
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
+struct Rope {
+    head: Coordinates,
+    tail: Coordinates,
+    tail_positions: HashSet<Coordinates>,
+}
+
+impl Rope {
+    fn new() -> Rope {
+        Rope {
+            head: Coordinates {
+                x: 0,
+                y: 0,
+            },
+            tail: Coordinates {
+                x: 0,
+                y: 0,
+            },
+            tail_positions: HashSet::new(),
+        }
+    }
+
+    fn adjust(&mut self, adjustment: Adjustment) {
+        for i in 0..adjustment.distance {
+            // Which way is the tail going to move?
+            /*let tail_direction = match self.head.orientation(self.tail) {
+                Direction::None => Direction::None, // Tail does not need to move
+                Direction::Up => Direction::Up,
+                Direction::Down => Direction::Down,
+
+            };*/
+            self.head.step(adjustment.direction);
+
+        }
+    }
+}
+
+
+#[derive(Debug, Copy, Clone, PartialEq)]
 struct Coordinates {
     x: isize,
     y: isize,
 }
 
-const UP: Coordinates = Coordinates {
-    x: 0,
-    y: -1,
-};
-const DOWN: Coordinates = Coordinates {
-    x: 0,
-    y: 1,
-};
-const LEFT: Coordinates = Coordinates {
-    x: -1,
-    y: 0,
-};
-const RIGHT: Coordinates = Coordinates {
-    x: 1,
-    y: 0,
-};
+impl Coordinates {
+    fn step(&mut self, direction: Direction) {
+        match direction {
+            Direction::Down => self.y += 1,
+            Direction::Up => self.y -= 1,
+            Direction::Left => self.x -= 1,
+            Direction::Right => self.x += 1,
+            Direction::DownRight => { self.step(Direction::Down); self.step(Direction::Right)},
+            Direction::DownLeft => { self.step(Direction::Down); self.step(Direction::Left)},
+            Direction::UpRight => { self.step(Direction::Up); self.step(Direction::Right)},
+            Direction::UpLeft => { self.step(Direction::Up); self.step(Direction::Left)},
+            Direction::None => {},
+        };
+    }
+
+    /* Returns the direction other would need to move to get closer to self */
+    fn orientation(&self, other: Coordinates) -> Direction {
+        let x = self.x - other.x;
+        let y = self.y - other.y;
+        if x == 0 && y == 0 {
+            Direction::None 
+        } else if x >= 1 {
+            if y == 0 {
+                Direction::Right
+            } else if y > 0 {
+                Direction::UpRight
+            } else {
+                Direction::DownRight
+            }
+        } else if x < 0 {
+            if y == 0 {
+                Direction::Left
+            } else if y > 0 {
+                Direction::UpLeft
+            } else {
+                Direction::DownLeft
+            }
+        } else  { // Implied x == 0
+            if y < 0 {
+                Direction::Down
+            } else {
+                Direction::Up
+            }
+        }
+    }
+}
+
+#[derive(Copy,Clone,PartialEq,Debug)]
+enum Direction {
+    None,
+    Up,
+    Down,
+    Left,
+    Right,
+    UpRight,
+    DownRight,
+    UpLeft,
+    DownLeft,
+}
+
+#[derive(Copy,Clone,PartialEq,Debug)]
+struct Adjustment {
+    direction: Direction,
+    distance: usize,
+}
+
+#[test]
+fn test_coordinates_orientation()
+{
+    let middle = Coordinates{x: 0, y: 0};
+    let middle2 = Coordinates{x: 0, y: 0};
+    assert_eq!(Direction::None, middle.orientation(middle2));
+    assert_eq!(Direction::None, middle2.orientation(middle));
+    let right = Coordinates{x:1, y: 0};
+    assert_eq!(Direction::Left, middle.orientation(right));
+    assert_eq!(Direction::Right, right.orientation(middle));
+    let left = Coordinates{x:-1, y:0};
+    assert_eq!(Direction::Right, middle.orientation(left));
+    assert_eq!(Direction::Left, left.orientation(middle));
+    let down = Coordinates{x:0, y:-1};
+    assert_eq!(Direction::Up, middle.orientation(down));
+    assert_eq!(Direction::Down, down.orientation(middle));
+    let up = Coordinates{x:0, y:1};
+    assert_eq!(Direction::Down, middle.orientation(up));
+    assert_eq!(Direction::Up, up.orientation(middle));
+    // Diagonals
+    assert_eq!(Direction::UpLeft, left.orientation(down));
+    assert_eq!(Direction::DownRight, down.orientation(left));
+    assert_eq!(Direction::UpRight, right.orientation(down));
+    assert_eq!(Direction::DownLeft, down.orientation(right));
+    // > 1 distance
+    assert_eq!(Direction::Right, right.orientation(left));
+    assert_eq!(Direction::Left, left.orientation(right));
+    assert_eq!(Direction::Up, up.orientation(down));
+    assert_eq!(Direction::Down, down.orientation(up));
+}
 
 fn parse_instructions(input: &str) -> Vec<Coordinates> {
     let coords = Vec::new();
@@ -303,4 +421,5 @@ R 2";
 
 fn main() {
     let buf = fs::read_to_string("2022d8p1.txt").unwrap();
+    println!("Directions: {:?}", parse_instructions(&buf));
 }
