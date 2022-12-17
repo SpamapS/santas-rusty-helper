79ddfffeb2b84a48517739f21545c3d3b346ee16
Clint Byrum <clint@fewbar.com>
2022-12-16 21:47:30 -0800

    d8p2 trust your feelings

--- 2022/d8p1/src/main.rs
+++ 2022/d8p1/src/main.rs
@@ -31,10 +31,51 @@ With 16 trees visible on the edge and another 5 visible in the interior, a total
 
 Consider your map; how many trees are visible from outside the grid?
 
+--- Part Two ---
+
+Content with the amount of tree cover available, the Elves just need to know the best spot to build their tree house: they would like to be able to see a lot of trees.
+
+To measure the viewing distance from a given tree, look up, down, left, and right from that tree; stop if you reach an edge or at the first tree that is the same height or taller than the tree under consideration. (If a tree is right on the edge, at least one of its viewing distances will be zero.)
+
+The Elves don't care about distant trees taller than those found by the rules above; the proposed tree house has large eaves to keep it dry, so they wouldn't be able to see higher than the tree house anyway.
+
+In the example above, consider the middle 5 in the second row:
+
+30373
+25512
+65332
+33549
+35390
+
+    Looking up, its view is not blocked; it can see 1 tree (of height 3).
+    Looking left, its view is blocked immediately; it can see only 1 tree (of height 5, right next to it).
+    Looking right, its view is not blocked; it can see 2 trees.
+    Looking down, its view is blocked eventually; it can see 2 trees (one of height 3, then the tree of height 5 that blocks its view).
+
+A tree's scenic score is found by multiplying together its viewing distance in each of the four directions. For this tree, this is 4 (found by multiplying 1 * 1 * 2 * 2).
+
+However, you can do even better: consider the tree of height 5 in the middle of the fourth row:
+
+30373
+25512
+65332
+33549
+35390
+
+    Looking up, its view is blocked at 2 trees (by another tree with a height of 5).
+    Looking left, its view is not blocked; it can see 2 trees.
+    Looking down, its view is also not blocked; it can see 1 tree.
+    Looking right, its view is blocked at 2 trees (by a massive tree of height 9).
+
+This tree's scenic score is 8 (2 * 2 * 1 * 2); this is the ideal spot for the tree house.
+
+Consider each tree on your map. What is the highest scenic score possible for any tree?
 
 */
 
 use std::{fs, cmp::max, fmt::{Formatter, Error, self}, collections::{HashMap, hash_map::{OccupiedEntry, VacantEntry}}};
+use itertools::iproduct;
+
 
 #[derive(PartialEq)]
 struct Grove {
@@ -117,6 +158,51 @@ impl Grove {
         left_visible | right_visible | up_visible | down_visible
     }
 
+    fn scenic_score(&self, x: usize, y: usize) -> usize {
+        let this_tree = self.get_tree(x, y);
+        // left
+        let mut left_score = 0;
+        if x > 0 {
+            for look_x in (0..x).rev() {
+                left_score += 1;
+                if self.get_tree(look_x, y) >= this_tree {
+                    break;
+                }
+            }
+        }
+        // right
+        let mut right_score = 0;
+        if x < self.width {
+            for look_x in x+1..self.width { 
+                right_score += 1;
+                if self.get_tree(look_x, y) >= this_tree {
+                    break;
+                }
+            }
+        }
+        // up
+        let mut up_score = 0;
+        if y > 0 {
+            for look_y in (0..=y-1).rev() {
+                up_score += 1;
+                if self.get_tree(x, look_y) >= this_tree {
+                    break;
+                }
+            }
+        }
+        // down
+        let mut down_score = 0;
+        if y < self.height {
+            for look_y in y+1..self.height {
+                down_score += 1;
+                if self.get_tree(x, look_y) >= this_tree {
+                    break;
+                }
+            }
+        }
+        left_score * right_score * up_score * down_score
+    }
+
     fn visible_trees(&self) -> usize {
         // Count the outer most ring
         let mut outer_visible_trees = (2*self.width) + (2*(self.height-2));
@@ -161,6 +247,18 @@ impl Grove {
         }
         visible_trees.iter().filter(|(_k, v)| **v).count() + outer_visible_trees
     }
+
+    fn best_scenic_score(&self) -> usize {
+        iproduct!(0..self.width, 0..self.height)
+            .map(|(x, y)| {
+                let s = self.scenic_score(x, y);
+                if s > self.width.pow(4) {
+                    println!("{},{} = {}", x, y, s)
+                }
+                s
+            })
+            .max().unwrap()
+    }
 }
 
 #[test]
@@ -197,9 +295,14 @@ fn test_parse_grid() {
     assert!(!parsed_grove.is_visible(1,3));
     assert!(!parsed_grove.is_visible(3,3));
     assert_eq!(21, parsed_grove.visible_trees());
+    assert_eq!(4, parsed_grove.scenic_score(2,1));
+    assert_eq!(8, parsed_grove.scenic_score(2,3));
+    assert_eq!(8, parsed_grove.best_scenic_score());
 }
 
 fn main() {
     let buf = fs::read_to_string("2022d8p1.txt").unwrap();
-    println!("Grove has {} visible trees.", parse_grove(&buf).visible_trees());
+    let grove = parse_grove(&buf);
+    println!("Grove has {} visible trees.", grove.visible_trees());
+    println!("The best scenic score possible is {}", grove.best_scenic_score());
 }
